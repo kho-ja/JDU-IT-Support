ba02b9d107b3969f132d569b12233a9958a482ca
kho_ja <azimxojayevabdulazizxoja@gmail.com>
2024-06-03 19:27:45 +0500

    4-dars

---
+++ Group JS/4-dars/script.js
@@ -0,0 +1,40 @@
+// // unary minus (negation)
+// let x = 1;
+
+// x = -x;
+// console.log(x);
+
+// // binary minus (subtraction)
+// let x = 1,
+//   y = 3;
+// console.log(y - x); // 2, binary minus subtracts values
+
+// // String concatenation with binary +
+// let s = "my" + "string";
+// console.log(s); // mystring
+
+// // Numeric conversion, unary +
+// // No effect on numbers
+// let x = 1;
+// console.log( +x ); // 1
+
+// let y = -2;
+// console.log( +y ); // -2
+
+// // Converts non-numbers
+// console.log( +true ); // 1
+// console.log( +"" );   // 0
+// console.log(+true); // 1
+// console.log(+"123123"); // 0
+
+// let apples = "2";
+// let oranges = "3";
+
+// // both values converted to numbers before the binary plus
+// console.log(+apples + +oranges); // 5
+
+// increment
+let counter = 1;
+let a = ++counter; // (*) changed ++counter to counter++
+
+console.log(a); // 1
